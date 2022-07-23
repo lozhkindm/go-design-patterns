d78cfd814d9b1fc83912b7de682f1c4bf6068896
Dmitriy <lozhkindm@yandex.ru>
2022-07-23 10:46:36 +0300

    singleton dependency inversion

--- singleton/dependency inversion/main.go
+++ singleton/dependency inversion/main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+type DB interface {
+	GetPopulation(name string) int
+}
+
+type dummyDB struct {
+	cities map[string]int
+}
+
+func (d *dummyDB) GetPopulation(name string) int {
+	if len(d.cities) == 0 {
+		d.cities = map[string]int{
+			"Moscow": 100,
+			"SPB":    250,
+		}
+	}
+	return d.cities[name]
+}
+
+func GetTotalPopulation(db DB, cities []string) int {
+	result := 0
+	for _, city := range cities {
+		result += db.GetPopulation(city) // problem solved, depending upon interface
+	}
+	return result
+}
+
+func main() {
+	cities := []string{"Moscow", "SPB"}
+	total := GetTotalPopulation(&dummyDB{}, cities)
+	ok := total == (100 + 250) // this test is ok and will not break
+	fmt.Println(ok)
 }
